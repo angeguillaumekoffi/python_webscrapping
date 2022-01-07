5bd5477b6f93208ae8c78e791315d0ab7599f336
Ange Guillaume Koffi <angekoffiguillaume@gmail.com>
2022-01-07 04:22:23 +0100

    project completed !

--- projet_fin_mod_python/settings.py
+++ projet_fin_mod_python/settings.py
@@ -125,6 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Parametrage du compte Email par defaut
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'angekoffiguillaume@gmail.com'
-EMAIL_HOST_PASSWORD = '00022501389440'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
